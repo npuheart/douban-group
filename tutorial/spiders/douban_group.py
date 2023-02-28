a59a920e156fb4f82e6a69e3ddeb4c6ec88036fe
shaoyaoqian <mpf_npu@icloud.com>
2023-02-28 10:59:11 +0800

    [ADD] 增加小组
    Former-commit-id: c93db6f82c0d472fb59a26ad96e9677ea980d40f

--- tutorial/spiders/douban_group.py
+++ tutorial/spiders/douban_group.py
@@ -6,34 +6,44 @@ class QuotesSpider(scrapy.Spider):
     name = "douban-group"
 
     def start_requests(self):
-        groups = ['icrush', '711609', '729203']
+        groups = ['icrush', '711609', '729203',
+                  '724924', 'make-it-clear', '652201',
+                  'guilv', '726559', 'EmirKusturica',
+                  '702958', '689047', 'kuakua',
+                  '733413', '687802', '732302']
+        # crush             装修灵感图库，              第三者视角观察自己，
+        # 亲密关系修复互助会   有一条无论如何都想沙一沙的龙  意难平小组
+        # 我发现个规律        围观工作党的爱情            佳片推荐
+        # 哄对象太难了吧      ta说这话什么意思            相互表扬小组
+        # 中二往事回忆录      不知道如何回复              我今天crush了没有回应
 
         url_base = 'https://www.douban.com/group/{group:s}/'
+        #           https://www.douban.com/group/guilv/discussion?start=50
 
         for group in groups:
             yield scrapy.Request(url=url_base.format(group=group), callback=self.parse, meta={'group': group})
 
     def parse(self, response):
-        filename = self.name + str(random.random())[-5:] +'.html'
+        filename = self.name + str(random.random())[-5:] + '.html'
         for tr in response.css('tr'):
             tds = tr.css('td')
             data = [tds[0].css('td a::attr(title)').get(),
-                tds[0].css('td a::attr(href)').get(),
-                tds[1].css('td a::text').get(),
-                tds[1].css('td a::attr(href)').get(),
-                tds[3].css('td::text').get()]
+                    tds[0].css('td a::attr(href)').get(),
+                    tds[1].css('td a::text').get(),
+                    tds[1].css('td a::attr(href)').get(),
+                    tds[3].css('td::text').get()]
             if '最后回应' not in data:
                 yield response.follow(url=data[1], callback=self.parse_item, meta=response.meta)
 
     def parse_item(self, response):
         item = dict()
-        item['filename'] = '博文' + str(random.random())[-5:] +'.html'
+        item['filename'] = '博文' + str(random.random())[-5:] + '.html'
         print(response.meta)
         item['group'] = response.meta['group']
         item['url'] = response.url
         item['title'] = response.css('.article h1').get()
         item['content'] = response.css('#link-report .topic-content').get()
-        item['author'] = response.css('.from a::text').get() 
+        item['author'] = response.css('.from a::text').get()
         item['author_link'] = response.css('.from a::attr(href)').get()
         item['create-time'] = response.css('.create-time::text').get()
         item['create-ip'] = response.css('.create-ip::text').get()
