3448f7cb1995dad9b70ca01922a6da3fead9c7c6
shaoyaoqian <mpf_npu@icloud.com>
2023-03-02 20:39:21 +0800

    .
    Former-commit-id: 5f2a117f84fd7987e0939985bb39aa1fd3bf30a2

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -27,12 +27,12 @@ DEFAULT_REQUEST_HEADERS = {
 # ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
