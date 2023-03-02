37de45638b5fd131457c4874de8496051c588f64
shaoyaoqian <mpf_npu@icloud.com>
2023-03-02 21:03:58 +0800

    .
    Former-commit-id: c303cecafb1aab3eb074ca86f29c952ec51c4f54 [formerly 26e3714d7e42b88872a9f170ae32a8e8fb256c99]
    Former-commit-id: f52559307a1c56f708db884e80ab6c2876a830af

--- tutorial/spiders/douban_group.py
+++ tutorial/spiders/douban_group.py
@@ -1,22 +1,12 @@
 import scrapy
 import random
-
+import os 
 
 class QuotesSpider(scrapy.Spider):
     name = "douban-group"
 
     def start_requests(self):
-        groups = ['icrush', '711609', '729203',
-                  '724924', 'make-it-clear', '652201',
-                  'guilv', '726559', 'EmirKusturica',
-                  '702958', '689047', 'kuakua',
-                  '733413', '687802', '732302']
-        # crush             装修灵感图库，              第三者视角观察自己，
-        # 亲密关系修复互助会   有一条无论如何都想沙一沙的龙  意难平小组
-        # 我发现个规律        围观工作党的爱情            佳片推荐
-        # 哄对象太难了吧      ta说这话什么意思            相互表扬小组
-        # 中二往事回忆录      不知道如何回复              我今天crush了没有回应
-
+        groups = [os.getenv('branch')]
         url_base = 'https://www.douban.com/group/{group:s}/'
         #           https://www.douban.com/group/guilv/discussion?start=50
 
