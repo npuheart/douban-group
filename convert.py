2d5339b529e53daddb406b2e9dec569a4ae55832
shaoyaoqian <mpf_npu@icloud.com>
2023-03-02 14:35:42 +0800

    a
    Former-commit-id: 20b26b0d52157553c5444b49dca1952ba929d188

--- convert.py
+++ convert.py
@@ -32,14 +32,14 @@ for group in r.smembers(redis_douban_group_all_groups):
         topic_comments = sorted(topic_comments)
         topic = {}
         topic['id'] = topic_id
-        topic['content'] = (topic_content or b'').decode('utf-8')
-        topic['title']= (topic_title or b'').decode('utf-8')
         topic['url'] =  (topic_url or b'').decode('utf-8')
-        topic['comments'] = topic_comments
+        topic['title']= (topic_title or b'').decode('utf-8')
         topic['author'] = (topic_author or b'').decode('utf-8')
         topic['author_link'] = (topic_author_link or b'').decode('utf-8')
         topic['create-time'] = (topic_created_time or b'').decode('utf-8')
         topic['create-ip'] = (topic_created_ip or b'').decode('utf-8')
+        topic['content'] = (topic_content or b'').decode('utf-8')
+        topic['comments'] = topic_comments
         data.append(topic)
     data = sorted(data, key=lambda s: s['create-time'], reverse=True)
     all_data[group] = data
